8d46a88bdab97c214125689ac68d9b9d8e743ebf
Sukey <35202440+sukey2008@users.noreply.github.com>
2023-07-29 12:39:06 +0200

    Create SimpleToken.sol

---
+++ SimpleToken.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.4;
+contract FunctionTypes{
+    uint256 public number = 5;
+    onstructor() payable {}
+    
+    function add() external{
+        number = number + 1;
+    }
